03e499ec303c646226b9f6fe35f5090a70d77a56
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-04-20 17:24:08 +0100

    commit

---
+++ _site/js/app.js
@@ -0,0 +1,12 @@
+// function animateBackground(){
+//    $('#introSection').ambience({
+//      time:3000,
+//      colors: ['black','darkgrey','darkblue', 'cyan','black','darkgrey','cyan','darkblue','cyan'],
+//      speed:"fast"
+//      }
+//      );
+//    };
+//
+//    animateBackground();
+//
+// console.log("hi from app.js");
